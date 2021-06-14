1e91d27ef50bf261f10f7f234ec6746448579c79
sakumashirayuki <2909549511@qq.com>
2021-06-14 22:43:49 +0800

    finish chart section

--- src/components/Charts/Bar3D.js
+++ src/components/Charts/Bar3D.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import FusionCharts from 'fusioncharts';
+import Charts from 'fusioncharts/fusioncharts.charts';
+import ReactFC from 'react-fusioncharts';
+import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
 
-const Bar3D = () => {
-  return <div>chart</div>;
+ReactFC.fcRoot(FusionCharts, Charts, FusionTheme);
+
+const Bar3D = ({data}) => {
+  const chartConfigs = {
+    type: 'bar3d',
+    width: '100%',
+    height: 400,
+    dataFormat: 'json',
+    dataSource: {
+      chart: {
+        caption: "Most Forked",
+        theme: "fusion",
+        xAxisName: "Repos",
+        xAxisNameFontSize: "16px",
+        yAxisName: "Stars",
+        yAxisNameFontSize: "16px",
+      },
+      data
+    },
+  };
+
+  return <ReactFC {...chartConfigs} />;
 };
 
 export default Bar3D;
--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,35 +1,83 @@
-import React from 'react';
-import styled from 'styled-components';
-import { GithubContext } from '../context/context';
-import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
+import React from "react";
+import styled from "styled-components";
+import { GithubContext } from "../context/context";
+import { Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts";
 const Repos = () => {
-  const { repos } = React.useContext(GithubContext);
-  console.log(repos);
-  return <h2>repos component</h2>;
+    const { repos } = React.useContext(GithubContext);
+    const languages = repos.reduce((total, item) => {
+        const { language, stargazers_count } = item;
+        if (!language)
+            // do nothing
+            return total;
+        if (!total[language])
+            total[language] = { value: 1, stars: stargazers_count };
+        else {
+            total[language] = {
+                ...total[language],
+                value: total[language].value + 1,
+                stars: total[language].stars + stargazers_count
+            };
+        }
+        return total;
+    }, {});
+    const languageData = [];
+    const starData = [];
+    for (const key in languages) {
+        languageData.push({
+            label: key,
+            value: languages[key].value.toString(),
+        });
+        starData.push({
+          label: key,
+          value: languages[key].stars.toString(),
+        });
+    }
+    languageData.sort((a, b) => b.value - a.value).slice(0, 5); // top 5
+    starData.sort((a, b) => b.stars - a.stars).slice(0, 5);
+
+    let {stars, forks} = repos.reduce((total, item)=>{ // only record one repo with the same stars
+      const {name, stargazers_count, forks} = item;
+      total.stars[stargazers_count] = {label: name, value: stargazers_count};
+      total.forks[forks] = {label: name, value: forks};
+      return total;
+    }, {stars: {}, forks: {}});
+
+    const repoData = Object.values(stars).slice(-5).reverse();
+    const forkData = Object.values(forks).slice(-5).reverse();
+
+    return (
+        <section className="section">
+            <Wrapper className="section-center">
+                <Pie3D data={languageData} />
+                <Column3D data={repoData}/>
+                <Doughnut2D data={starData} />
+                <Bar3D data={forkData}/>
+            </Wrapper>
+        </section>
+    );
 };
 
 const Wrapper = styled.div`
-  display: grid;
-  justify-items: center;
-  gap: 2rem;
-  @media (min-width: 800px) {
-    grid-template-columns: 1fr 1fr;
-  }
+    display: grid;
+    justify-items: center;
+    gap: 2rem;
+    @media (min-width: 800px) {
+        grid-template-columns: 1fr 1fr;
+    }
 
-  @media (min-width: 1200px) {
-    grid-template-columns: 2fr 3fr;
-  }
-
-  div {
-    width: 100% !important;
-  }
-  .fusioncharts-container {
-    width: 100% !important;
-  }
-  svg {
-    width: 100% !important;
-    border-radius: var(--radius) !important;
-  }
+    @media (min-width: 1200px) {
+        grid-template-columns: 2fr 3fr;
+    }
+    div {
+        width: 100% !important;
+    }
+    .fusioncharts-container {
+        width: 100% !important;
+    }
+    svg {
+        width: 100% !important;
+        border-radius: var(--radius) !important;
+    }
 `;
 
 export default Repos;
